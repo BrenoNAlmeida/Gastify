00e989c8bbce66f2231ea6bdc679f75ecb2f3781
Breno Nascimento de Almeida <72624554+BrenoNAlmeida@users.noreply.github.com>
2024-04-25 23:13:29 -0300

    Revert "Revert "46 padrão de codificação com black""

--- gastify_api/views.py
+++ gastify_api/views.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import usuario, forma_pagamento, despesa
-from .serializers import UsuarioSerializer, FormaPagamentoSerializer, DespesaSerializer 
+from .serializers import UsuarioSerializer, FormaPagamentoSerializer, DespesaSerializer
 from django.http import HttpResponse
 from rest_framework import status, viewsets
 
