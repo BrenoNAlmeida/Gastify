c56f498b28979ea5f0e0a9fc6d2415246945d920
Breno Nascimento de Almeida <72624554+BrenoNAlmeida@users.noreply.github.com>
2024-04-25 23:08:07 -0300

    Revert "46 padrão de codificação com black"

--- gastify_api/apps.py
+++ gastify_api/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class GastifyApiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "gastify_api"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gastify_api'
